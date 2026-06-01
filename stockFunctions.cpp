5b355890dec7889e7ad85fb8a613642d45e1dca9
A Student <student@nowhere.edu>
2026-06-01 20:13:54 +0000

    Jun 1, 2026, 1:13 PM

--- stockFunctions.cpp
+++ stockFunctions.cpp
@@ -4,3 +4,12 @@
 #include <vector>
 
 using namespace std;
+
+double percentChange(const vector<double>& values, int startDay, int endDay)
+{
+  double startValue = values[startDay];
+  double endValue = values[endDay];
+  double change = endValue - startValue;
+
+  return change / startValue * 100;
+}
